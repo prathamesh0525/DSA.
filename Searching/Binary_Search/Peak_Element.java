7b82261672ce735f6a72c5ae29a04fec4d6dd439
prathamesh0525 <prathameshkale2333@gmail.com>
2025-04-27 16:44:53 +0530

    Update Peak_Element.java

--- Searching/Binary_Search/Peak_Element.java
+++ Searching/Binary_Search/Peak_Element.java
@@ -1,19 +1,7 @@
 package Searching.Binary_Search;
 
 class peak_Element{
-   // public static int Peak(int arr[]){
-
-        // time complexity O(n)
-
-    //     int peak=0;
-        
-    //     for(int i=1;i<arr.length-1;i++){
-    //         if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
-    //             peak=i;
-    //         }
-
-    //     }
-
+   
     public static int Peak(int arr[]){
     int peak=0;
     int start=1;
